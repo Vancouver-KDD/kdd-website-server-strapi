468221883c88009b33ac8555f0f6ed135a564a4d
jka236 <jka236@sfu.ca>
2021-08-17 15:06:35 -0700

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'fee92769b2db814669fb4dbae9626f2f'),
-    },
-  },
+  url: env('MY_HEROKU_URL'),
 });
+
+ 
+
+ 
