62378b02e0f92b860b253b14ec221aac918853da
jka236 <jka236@sfu.ca>
2021-08-17 17:26:10 -0700

    jwt updated

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -3,36 +3,93 @@
 // };
 
 const _ = require('lodash');
-const axios = require('axios');
 
 module.exports = async (ctx, next) => {
   let role;
 
+  if (ctx.state.user) {
+    // request is already authenticated in a different way
+    return next();
+  }
+
   if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
     try {
-      const { id, isAdmin = false } = await strapi.plugins[
-        'users-permissions'
-      ].services.jwt.getToken(ctx);
+      const { id } = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx);
 
-      // ...
-
-    } catch (err) {
-      try {
-        const data = await axios({
-          method: 'post',
-          url: 'http://YOUR_DOMAIN/userinfo',
-          headers: {
-            Authorization: ctx.request.header.authorization
-          }
-        });
-
-        // if you want do more validation test
-        // feel free to add your code here.
-
-        return await next();
-      } catch (error) {
-        return handleErrors(ctx, new Error('Invalid token: Token did not match with Strapi and Auth0'), 'unauthorized');
+      if (id === undefined) {
+        throw new Error('Invalid token: Token did not contain required fields');
       }
+
+      // fetch authenticated user
+      ctx.state.user = await strapi.plugins[
+        'users-permissions'
+      ].services.user.fetchAuthenticatedUser(id);
+    } catch (err) {
+      return handleErrors(ctx, err, 'unauthorized');
+    }
+
+    if (!ctx.state.user) {
+      return handleErrors(ctx, 'User Not Found', 'unauthorized');
+    }
+
+    role = ctx.state.user.role;
+
+    if (role.type === 'root') {
+      return await next();
+    }
+
+    const store = await strapi.store({
+      environment: '',
+      type: 'plugin',
+      name: 'users-permissions',
+    });
+
+    if (
+      _.get(await store.get({ key: 'advanced' }), 'email_confirmation') &&
+      !ctx.state.user.confirmed
+    ) {
+      return handleErrors(ctx, 'Your account email is not confirmed.', 'unauthorized');
+    }
+
+    if (ctx.state.user.blocked) {
+      return handleErrors(
+        ctx,
+        'Your account has been blocked by the administrator.',
+        'unauthorized'
+      );
     }
   }
-}
+
+  // Retrieve `public` role.
+  if (!role) {
+    role = await strapi.query('role', 'users-permissions').findOne({ type: 'public' }, []);
+  }
+
+  const route = ctx.request.route;
+  const permission = await strapi.query('permission', 'users-permissions').findOne(
+    {
+      role: role.id,
+      type: route.plugin || 'application',
+      controller: route.controller,
+      action: route.action,
+      enabled: true,
+    },
+    []
+  );
+
+  if (!permission) {
+    return handleErrors(ctx, undefined, 'forbidden');
+  }
+
+  // Execute the policies.
+  if (permission.policy) {
+    return await strapi.plugins['users-permissions'].config.policies[permission.policy](ctx, next);
+  }
+
+  // Execute the action.
+  await next();
+};
+
+const handleErrors = (ctx, err = undefined, type) => {
+  throw strapi.errors[type](err);
+};
